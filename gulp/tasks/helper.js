0dda471991a909abd63f6e75f418392094c460fd
AnderEsso <andr03409@gmail.com>
2021-11-24 12:42:43 +0200

    added fix for git-hub pages

--- gulp/tasks/helper.js
+++
@@ -1,8 +0,0 @@
-var gulp = require( 'gulp' );
-var del = require( 'del' );
-
-
-// clean build directory
-gulp.task( 'clean', function() {
-	return del( 'build' );
-});
--- src/js/libs.js
+++
@@ -1,3 +0,0 @@
-//=include ../lib/inputmask/dist/jquery.inputmask.min.js
-//=include ../lib/select2/dist/js/select2.full.min.js
-//=include ../lib/select2/dist/js/i18n/uk.js
